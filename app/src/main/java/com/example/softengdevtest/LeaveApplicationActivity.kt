eeea48e59291846dc212fed93e7faa9eb26d3c9d
Ainslee <codedearstwozero@gmail.com>
2025-03-04 09:58:37 +0800

    added dialog for save btn

--- app/src/main/java/com/example/softengdevtest/LeaveApplicationActivity.kt
+++ app/src/main/java/com/example/softengdevtest/LeaveApplicationActivity.kt
@@ -1,11 +1,31 @@
 package com.example.softengdevtest
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class LeaveApplicationActivity : AppCompatActivity() {
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
       setContentView(R.layout.activity_leave_application01)
+
+      findViewById<Button>(R.id.savebtn).setOnClickListener {
+         val intent = Intent(this, LeaveMonitor ::class.java)
+
+         val builder = AlertDialog.Builder(this)
+            .setTitle("Leave Application")
+            .setMessage("Are you sure you want to submit the leave application?")
+            .setPositiveButton("Yes"){_, _->
+               startActivity(intent)
+            }
+            .setNegativeButton("No"){dialog, _->
+               dialog.dismiss()
+            }
+         builder.create().show()
+
+      }
+
    }
 }
